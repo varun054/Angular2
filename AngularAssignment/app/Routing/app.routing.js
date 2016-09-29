dfad9370916179e5162b973644ad6e83c1bba131
varun054 <varunmcabbd@gmail.com>
2016-09-29 18:40:46 +0530

    updated code
    updated code

--- AngularAssignment/app/Routing/app.routing.js
+++ AngularAssignment/app/Routing/app.routing.js
@@ -4,10 +4,12 @@ var dashboard_component_1 = require('../Component/Dashboard-Component/dashboard.
 var about_component_1 = require('../Component/About-Component/about.component');
 var contact_component_1 = require('../Component/Contact-Component/contact.component');
 var employee_component_1 = require('../Component/Employee-Component/employee.component');
+var empdetail_component_1 = require('../Component/Employee-Component/empdetail.component');
 var appRoutes = [
     { path: '', component: dashboard_component_1.DashboardComponent },
     { path: 'dashboard', component: dashboard_component_1.DashboardComponent },
     { path: 'addemp', component: employee_component_1.EmployeeComponent },
+    { path: 'empdetail/:empid', component: empdetail_component_1.EmpdetailComponent },
     { path: 'about', component: about_component_1.AboutComponent },
     { path: 'contact', component: contact_component_1.ContactComponent }
 ];
--- AngularAssignment/app/Routing/app.routing.ts
+++ AngularAssignment/app/Routing/app.routing.ts
@@ -4,11 +4,13 @@ import { DashboardComponent }  from '../Component/Dashboard-Component/dashboard.
 import { AboutComponent }  from '../Component/About-Component/about.component';
 import { ContactComponent }    from '../Component/Contact-Component/contact.component';
 import { EmployeeComponent }    from '../Component/Employee-Component/employee.component';
+import { EmpdetailComponent }    from '../Component/Employee-Component/empdetail.component';
 
 const appRoutes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'addemp', component: EmployeeComponent },
+  { path: 'empdetail/:empid', component: EmpdetailComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent }
 ];
